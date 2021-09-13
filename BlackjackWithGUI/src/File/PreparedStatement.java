1d6b335853ccf3af634ded0a6fb90308a2186cb7
mrb34n135421234 <70122753+mrb34n135421234@users.noreply.github.com>
2021-09-14 04:54:15 +1200

    Create updateHuman()

--- BlackjackWithGUI/src/File/PreparedStatement.java
+++ BlackjackWithGUI/src/File/PreparedStatement.java
@@ -4,10 +4,59 @@
  */
 package File;
 
+import Players.HumanPlayer;
+import java.sql.SQLException;
+import java.util.List;
+
 /**
  *
  * @author Jason Christian - 21136899
  */
 public class PreparedStatement {
 
+    DBManager dbManager;
+
+    public PreparedStatement() {
+        dbManager = new DBManager();
+    }
+
+    public int updateHuman(HumanPlayer human) {
+        int rowCount = 0;
+        java.sql.PreparedStatement pstmt;
+        String sql = "UPDATE PLAYERS SET CHIP=? WHERE NAME=?";
+        try {
+            pstmt = dbManager.getConnection().prepareStatement(sql);
+            pstmt.setString(2, human.getName());
+            pstmt.setDouble(1, human.getTotalGain());
+            //How many rows are affected
+            int executeUpdate = pstmt.executeUpdate();
+            return executeUpdate;
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+        return rowCount;
+    }
+
+    public static void main(String args[]) {
+        RetrieveAll retrieve = new RetrieveAll();
+        List<HumanPlayer> humanList = retrieve.getAllCars();
+
+        //Update the third element of carList, id=3
+        HumanPlayer humanToUpdate = humanList.get(1);
+        humanToUpdate.setTotalGain(35);
+
+        //Fire update
+        PreparedStatement prepared = new PreparedStatement();
+        int updateResult = prepared.updateHuman(humanToUpdate);
+        System.out.println("Update Result: " + updateResult);
+        prepared.dbManager.closeConnections();
+
+        //retrieve update
+        HumanPlayer humanUpdated = retrieve.getPlayerByName("'Benny'");
+        retrieve.dbManager.closeConnections();
+        System.out.println("Update player name: " + humanUpdated.getName() + " total chips to: " + humanUpdated.getTotalGain());
+        for (HumanPlayer human : humanList) {
+            System.out.println("Name: " + human.getName() + ", Chips: " + human.getTotalGain());
+        }
+    }
 }
