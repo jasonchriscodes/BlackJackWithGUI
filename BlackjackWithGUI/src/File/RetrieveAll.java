1274f919272a3bd0701b76acda1422f879e68d73
mrb34n135421234 <70122753+mrb34n135421234@users.noreply.github.com>
2021-09-13 15:59:07 +1200

    Create RetrieveAll class

---
+++ BlackjackWithGUI/src/File/RetrieveAll.java
@@ -0,0 +1,15 @@
+/*
+ * Class represents a converter the result set to an object / object list.
+ * Sometimes, methods require an object reference, such as car, you may
+ * not able to pass the result set as the argument for this method, therefore,
+ * we normally "convert" the result set to an object / object list
+ */
+package File;
+
+/**
+ *
+ * @author Jason Christian - 21136899
+ */
+public class RetrieveAll {
+
+}
