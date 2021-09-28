304c8af9e5ae4c6fcbf528f64fac3b41c2f085bd
mrb34n135421234 <70122753+mrb34n135421234@users.noreply.github.com>
2021-09-29 00:34:22 +1300

    Create a basic frame of the game

--- BlackjackWithGUI/src/Graphic/BlackJackFrame.java
+++ BlackjackWithGUI/src/Graphic/BlackJackFrame.java
@@ -3,10 +3,49 @@
  */
 package Graphic;
 
+import java.awt.*;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import javax.swing.*;
+
 /**
  *
  * @author Jason Christian - 21136899
  */
-public class BlackJackFrame {
+public class BlackjackFrame extends JFrame {
 
+    /**
+     * The Constant DEFAULT_WIDTH.
+     */
+    public static final int DEFAULT_WIDTH = 450;
+
+    /**
+     * The Constant DEFAULT_HEIGHT.
+     */
+    public static final int DEFAULT_HEIGHT = 600;
+
+    /**
+     * The Constant cardLayout.
+     */
+    public static final CardLayout cardLayout = new CardLayout();
+
+    /**
+     * Instantiates a new blackjack frame.
+     */
+    public BlackjackFrame() {
+        setSize(DEFAULT_WIDTH, DEFAULT_HEIGHT);
+        setLocationRelativeTo(null);
+        setLayout(cardLayout);
+        setTitle("Blackjack");
+        setResizable(false);
+
+        this.addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent e) {
+                super.windowClosing(e);
+                JOptionPane.showMessageDialog(null, "Exiting game!\nYour record will be saved!", "Information", JOptionPane.INFORMATION_MESSAGE);
+                System.exit(0);
+            }
+        });
+    }
 }
