43f5208f5b4866affc984d40fec2b40db90ad1e7
mrb34n135421234 <70122753+mrb34n135421234@users.noreply.github.com>
2021-10-01 23:49:11 +1300

    Create DarkPalette class implements Palette

---
+++ BlackjackWithGUI/src/Graphic/DarkPalette.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Graphic;
+
+import java.awt.Color;
+
+/**
+ *
+ * @author Jason Christian - 21136899
+ */
+public class DarkPalette implements Palette {
+
+    @Override
+    public Color background() {
+        return new Color(3, 3, 3);
+    }
+
+    @Override
+    public Color menu() {
+        return new Color(26, 26, 27);
+    }
+
+    @Override
+    public Color separator() {
+        return new Color(52, 53, 54);
+    }
+
+    @Override
+    public Color heading() {
+        return new Color(129, 131, 132);
+    }
+
+    @Override
+    public Color text() {
+        return Color.WHITE;
+    }
+
+    @Override
+    public Color button() {
+        return new Color(215, 218, 220);
+    }
+
+    @Override
+    public Color altButton() {
+        return new Color(39, 39, 41);
+    }
+}
