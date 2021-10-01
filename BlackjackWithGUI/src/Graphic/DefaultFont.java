6e4c4936657b879978246bb8c3c7c15a48806603
mrb34n135421234 <70122753+mrb34n135421234@users.noreply.github.com>
2021-10-01 21:57:20 +1300

    Create DefaultFont class

---
+++ BlackjackWithGUI/src/Graphic/DefaultFont.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Graphic;
+
+/**
+ *
+ * @author Jason Christian - 21136899
+ */
+public class DefaultFont {
+
+    private final String name;
+
+    /**
+     * Creates a new {@code DefaultFont} with the specified font name.
+     *
+     * @param name the font name
+     */
+    public DefaultFont(String name) {
+        this.name = name;
+    }
+
+    /**
+     * Returns a regular-styled font with the specified size.
+     *
+     * @param size the point size of the font
+     * @return the font
+     */
+    public Font generate(int size) {
+        return generate(size, Font.PLAIN);
+    }
+
+    /**
+     * Returns a font with the specified size and style.
+     *
+     * @param size the point size of the font
+     * @param style the style of the font
+     * @return the font
+     */
+    public Font generate(int size, int style) {
+        return new Font(name, style, size);
+    }
+
+}
