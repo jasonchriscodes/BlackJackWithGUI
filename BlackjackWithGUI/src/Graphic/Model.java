8ea6c10151ad9c2c1e8f7e70e2f6a1f3c11de2d0
mrb34n135421234 <70122753+mrb34n135421234@users.noreply.github.com>
2021-10-01 21:12:47 +1300

    Create CHIPS[]

--- BlackjackWithGUI/src/Graphic/Model.java
+++ BlackjackWithGUI/src/Graphic/Model.java
@@ -11,4 +11,21 @@ package Graphic;
  */
 public class Model {
 
+    private static final int[] CHIPS = {100, 50, 25, 10, 5};
+
+    /**
+     * Returns the possible types of chips a player may bet.
+     *
+     * <p>
+     * Every int element in the private array {@code CHIPS} should be displayed
+     * on the screen. A note about the order of the elements in the array: the
+     * last element must be the smallest value. This is to keep the rightmost
+     * insets intact as a chip option get disabled.
+     *
+     * @return the chips
+     */
+    public static final int[] chips() {
+        return CHIPS.clone();
+    }
+
 }
