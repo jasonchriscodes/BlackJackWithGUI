20952eeb78ec81da9d2bc1cf895c68ada031d20e
mrb34n135421234 <70122753+mrb34n135421234@users.noreply.github.com>
2021-10-01 21:12:56 +1300

    Create initBetOptions()

--- BlackjackWithGUI/src/Graphic/View.java
+++ BlackjackWithGUI/src/Graphic/View.java
@@ -5,10 +5,24 @@
  */
 package Graphic;
 
+import java.util.Map;
+import javax.swing.*;
+
 /**
  *
  * @author Jason Christian - 21136899
  */
 public class View {
+
+    private final Map<String, JButton> betOptions;
+    private final JPanel betOptionsPanel;
+
+    public void initBetOptions(int[] options) {
+        String[] stringOptions = new String[options.length];
+        for (int i = 0; i < stringOptions.length; i++) {
+            stringOptions[i] = String.valueOf(options[i]);
+        }
+        initOptions("CHIPS", stringOptions, betOptions, betOptionsPanel, true);
+    }
 
 }
