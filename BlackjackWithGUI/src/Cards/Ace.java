be17e18ccce954b5a291a3e8f1c7d06ae40cbdbc
mrb34n135421234 <70122753+mrb34n135421234@users.noreply.github.com>
2021-10-03 16:02:37 +1300

    Create Ace class

---
+++ BlackjackWithGUI/src/Cards/Ace.java
@@ -0,0 +1,30 @@
+/*
+ * Class represents ace card
+ */
+package Cards;
+
+/**
+ *
+ * @author Jason Christian - 21136899
+ */
+public class Ace extends Card {
+
+    /**
+     * Creates a new {@code Ace} with the specified suit.
+     *
+     * <p>
+     * An ace has a value of 1, however, it may also be counted as 11 in a game
+     * of blackjack.
+     *
+     * @param suit possible suit values are: {@code SPADES}, {@code HEARTS},
+     * {@code CLUBS} and {@code DIAMONDS}
+     */
+    public Ace(Suit suit) {
+        super(1, suit);
+    }
+
+    @Override
+    public String toString() {
+        return "Ace of " + getSuit();
+    }
+}
