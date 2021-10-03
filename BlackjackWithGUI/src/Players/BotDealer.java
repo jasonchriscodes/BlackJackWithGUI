0eb7ae813f16129e9a044b596b9737341bf047d5
mrb34n135421234 <70122753+mrb34n135421234@users.noreply.github.com>
2021-10-03 16:32:32 +1300

    Create BotDealer() constructor and hasSoft17()

--- BlackjackWithGUI/src/Players/BotDealer.java
+++ BlackjackWithGUI/src/Players/BotDealer.java
@@ -25,6 +25,26 @@ public class BotDealer extends Participant {
     }
 
     /**
+     * Another botDealer constructor
+     */
+    public BotDealer() {
+        super("Dealer");
+    }
+
+    /**
+     * Determines if this dealer has a soft 17.
+     *
+     * <p>
+     * A hand that contains an ace counted as 11 is a soft hand. If the hand is
+     * soft and has a value of 17, it is a soft 17.
+     *
+     * @return true if this dealer has a soft 17
+     */
+    public boolean hasSoft17() {
+        return getHandValue() == 17 && hasSoftHand();
+    }
+
+    /**
      * Dealer score
      *
      * @return
