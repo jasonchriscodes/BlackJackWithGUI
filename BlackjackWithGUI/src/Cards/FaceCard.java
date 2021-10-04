6d47841abda0a762f756a331b11679a5086762a3
mrb34n135421234 <70122753+mrb34n135421234@users.noreply.github.com>
2021-10-04 22:59:58 +1300

    Create FaceCard class

---
+++ BlackjackWithGUI/src/Cards/FaceCard.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Cards;
+
+/**
+ *
+ * @author Jason Christian - 21136899
+ */
+public class FaceCard extends Card {
+
+    /**
+     * Creates a new {@code FaceCard} with the specified face and suit.
+     *
+     * <p>
+     * In a standard 52-card deck, face cards are cards that depict a person
+     * instead of symbols on the front side of the card. Every face cards has a
+     * value of 10.
+     *
+     * @param face possible face values are: {@code KING}, {@code QUEEN},
+     * {@code JACK}
+     * @param suit possible suit values are: {@code SPADES}, {@code HEARTS},
+     * {@code CLUBS} and {@code DIAMONDS}
+     */
+    public FaceCard(Face face, Suit suit) {
+        super(10, suit);
+        this.face = face;
+    }
+
+    /**
+     * Returns the face of this card.
+     *
+     * @return the face of this card
+     */
+    public Face getFace() {
+        return face;
+    }
+
+    @Override
+    public String toString() {
+        return face + " of " + getSuit();
+    }
+
+    private final Face face;
+}
